1f166031b48afc2b56e9343a92b8a1f7bb4061ee
Sasivardhan gmail <sasivardhan876@gmail.com>
2025-08-12 17:37:00 +0530

    sasivardhangithub

---
+++ sasivardhan/Anagram.py
@@ -0,0 +1,6 @@
+s1=input("Enter the first str: ")
+s2=input("Enter the second str: ")
+if s1==s2:
+    print("Anagram")
+else:
+    print("Not anagram")
---
+++ sasivardhan/palindrome.py
@@ -0,0 +1,6 @@
+x=input("Enter the str: ")
+a=x[::-1]
+if x==a:
+    print("palindrome")
+else:
+    print("Not a palindrome")
