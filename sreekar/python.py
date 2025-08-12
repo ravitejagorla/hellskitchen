89db992fca09f76a883c09a4788844c0b0546bd3
Sreekar45 <sreesreekar50@gmail.com>
2025-08-12 17:28:20 +0530

    python files

--- sreekar/python.py
+++
@@ -1,9 +0,0 @@
-def is_prime(n):
-    return n > 1 and all(n % i for i in range(2, int(n**0.5)+1))
-
-def is_palindrome(s):
-    s = s.lower().replace(" ", "")
-    return s == s[::-1]
-
-def are_anagrams(a, b):
-    return sorted(a.replace(" ", "").lower()) == sorted(b.replace(" ", "").lower())
