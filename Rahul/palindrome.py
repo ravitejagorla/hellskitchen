d542676f81147de41423034977fba9d246b8fdec
Rahulsharma0991 <rahulsharma077074@gmail.com>
2025-08-12 17:38:38 +0530

    assignment
    pushing folder

---
+++ Rahul/palindrome.py
@@ -0,0 +1,6 @@
+#  ===========================PALINDROME====================
+a=input("Enter anythig")
+if a==a[::-1]:
+    print("Its a palindrome number")
+else:
+    print("not a palindrome number")
---
+++ Rahul/prime.py
@@ -0,0 +1,18 @@
+# =======================PRIME NUMBER=======================
+a = int(input("Enter the Number: "))
+for i in range(2, a):
+    if a % i == 0:
+        print("Not a prime number")
+        break
+else:
+    print("Prime number")
+
+
+# =======================PRIME NUMBER=======================
+a = int(input("Enter the Number: "))
+for i in range(2, a):
+    if a % i == 0:
+        print("Not a prime number")
+        break
+else:
+    print("Prime number")
