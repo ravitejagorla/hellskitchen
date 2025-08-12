7d87f6d2bf436d0b680a65b661c43c32bb23f4ed
Ravi Teja Gorla <115577841+ravitejagorla@users.noreply.github.com>
2025-08-12 17:23:16 +0530

    Delete gafur directory
    deleting extra

--- gafur/prime.py
+++
@@ -1,9 +0,0 @@
-a=int(input("Enter any number: "))
-for i in range(2,a+1):
-    pr=True
-    for j in range(2,i):
-        if i%j==0:
-            pr=False
-            break
-    if pr:
-        print(i,end=" ")
