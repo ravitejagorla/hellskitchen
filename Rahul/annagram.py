99a2fe92b5c11aee04c8710d53d6d8e1314888e1
Ravi Teja Gorla <115577841+ravitejagorla@users.noreply.github.com>
2025-08-12 17:52:05 +0530

    Delete Rahul directory

--- Rahul/annagram.py
+++
@@ -1,7 +0,0 @@
-# =======================ANAGRAM==========================
-str1=input("Enter anything  :")
-str2=input("Enter the anything  :")
-if sorted(str1)==sorted(str2):
-    print("its a annagram")
-else:
-    print("not a annagram")
--- Rahul/palindrome.py
+++
@@ -1,6 +0,0 @@
-#  ===========================PALINDROME====================
-a=input("Enter anythig")
-if a==a[::-1]:
-    print("Its a palindrome number")
-else:
-    print("not a palindrome number")
