61ba83392a8690dddf1fbb2ee35f546e16a0ccdc
prashant-210110 <prashantnagasai@gmail.com>
2025-08-13 16:25:35 +0530

    Create Anagram.py

---
+++ prashant/Anagram.py
@@ -0,0 +1,7 @@
+#printing anagram or not
+a=input("enter the first value or string :")
+b=input("enter the second value or string :")
+if a==b:
+  print(f"{a} is anagram")
+else:
+  print(f"{a} is not anagram")
